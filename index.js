d178a8efd8df9e77d94f0053333151f786675ec1
Utkarsh <utkarshmishra2001@gmail.com>
2023-03-17 20:35:51 +0530

    Allowed CORS from localhost

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const userRouter = require("./routes/user");
 const client = connectToDatabase(process.env.MONGO_URI);
+const cors = cors();
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1", userRouter);
 if (client) {
