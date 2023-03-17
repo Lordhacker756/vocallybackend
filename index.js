eb0d8ec54ff5b597b52f2e01e3bae1d720dbd4d3
Utkarsh <utkarshmishra2001@gmail.com>
2023-03-18 00:20:00 +0530

    cors policy fixes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const { connectToDatabase } = require("./utils/ConnectToDatabase");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const userRouter = require("./routes/user");
 const client = connectToDatabase(process.env.MONGO_URI);
-const cors = cors();
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1", userRouter);
